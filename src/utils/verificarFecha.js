67ae14aedce7cc2ba0fec2a791d2408070093113
luciano <sessaregoluchi@gmail.com>
2023-08-17 11:06:37 -0300

    pabloo

--- src/utils/verificarFecha.js
+++ src/utils/verificarFecha.js
@@ -2,13 +2,13 @@ export function verificarFecha(usuario){
     const fechaaLimite = new Date()
     fechaaLimite.setHours(fechaaLimite.getHours()-3)
     //fechaaLimite.setDate(fechaaLimite.getDate()-2)
-    fechaaLimite.setDate(fechaaLimite.getDate()-3)
+    fechaaLimite.setDate(fechaaLimite.getDate()-2)
     console.log("limite: ", fechaaLimite)
     ///////////
     console.log("UT: ", usuario.last_connection)
     const fechaConvertida = new Date(usuario.last_connection)
 
-    console.log("convertida: ", fechaaLimite)
+    console.log("convertida: ", fechaConvertida)
     if(fechaaLimite > fechaConvertida){
         console.log("este tiene q ser borrado")
         //return true
